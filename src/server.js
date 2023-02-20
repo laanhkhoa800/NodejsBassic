23e12c0bcfff8c57339b888a0051e6fe36a4fdc3
laanhkhoa800 <laanhkhoa800@gmail.com>
2023-02-20 16:18:09 +0700

    up code use midlewere create notfoud page

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 import initWebRoute from './route/web';
 import initApiRoute from './route/api';
 import configViewEngine from './config/viewEngine';
-import connection from './config/connectDB';
 
 require('dotenv').config();
 
 const express = require('express')
 const path = require('path');
+var morgan = require('morgan');
 
 const app = express()
 const port = process.env.PORT || 8080;
 
+app.use(morgan('combined'));
+
 //config to get data from request UI up controller  
+// use midlewere to conver res to JSON and object
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-  
+
 //setup view engine
 configViewEngine(app);
 
@@ -22,6 +25,11 @@ configViewEngine(app);
 initWebRoute(app);
 //init api route
 initApiRoute(app);
+
+//use midlewere handle 404 not found
+app.use( (req, res) => {
+  return res.render('notFound.ejs');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
